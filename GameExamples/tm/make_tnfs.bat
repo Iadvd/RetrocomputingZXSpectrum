9c86a353df8676a8b7fe56c754f7c4b2699ad309
Iadvd <42761379+Iadvd@users.noreply.github.com>
2019-04-19 13:17:25 +0900

    Add files via upload

---
+++ GameExamples/tm/make_tnfs.bat
@@ -0,0 +1,12 @@
+del loader.tap
+del screen.tap
+del %1.tap
+del main.tap
+zcc +zx -vn %1.c -o %1.bin -lndos -lsplib2 -llibsocket -create-app -zorg=24000
+.\utils\bas2tap.exe -a10 -sTENEBRA .\loader_tnfs.bas .\loader.tap
+.\utils\bin2tap.exe -o .\screen.tap -a 16384 .\loading.bin
+.\utils\bin2tap.exe -o .\main.tap -a 24000 .\%1.bin
+copy /b .\loader.tap + .\main.tap .\%1.tap
+echo DONE
+
+
